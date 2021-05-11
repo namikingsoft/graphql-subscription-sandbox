73246b858414a8b29affcc67218adcd6966f2c76
Tsubasa Namiki <namikingsoft@gmail.com>
2021-05-12 06:29:58 +0900

    Generate api client

--- Makefile
+++ Makefile
@@ -1,2 +1,6 @@
+.PHONY: src/client/lib/api.ts
+src/client/lib/api.ts:
+	npx graphql-codegen --config $(CURDIR)/gqlcodegen.yml
+
 psql:
 	docker-compose exec database psql "postgresql://postgres:postgres@database:5432/postgres"
---
+++ src/client/lib/api.ts
@@ -0,0 +1,138 @@
+import { gql } from '@apollo/client';
+import * as Apollo from '@apollo/client';
+export type Maybe<T> = T | null;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+const defaultOptions =  {}
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+};
+
+export type Message = {
+  __typename?: 'Message';
+  id: Scalars['ID'];
+  text: Scalars['String'];
+  createdAt: Scalars['Float'];
+};
+
+export type MessageInput = {
+  text: Scalars['String'];
+};
+
+export type Mutation = {
+  __typename?: 'Mutation';
+  postMessage: Message;
+};
+
+
+export type MutationPostMessageArgs = {
+  messageInput: MessageInput;
+};
+
+export type Query = {
+  __typename?: 'Query';
+  messages: Array<Message>;
+};
+
+export type MessagesQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+export type MessagesQuery = (
+  { __typename?: 'Query' }
+  & { messages: Array<(
+    { __typename?: 'Message' }
+    & Pick<Message, 'id' | 'text' | 'createdAt'>
+  )> }
+);
+
+export type PostMessageMutationVariables = Exact<{
+  messageInput: MessageInput;
+}>;
+
+
+export type PostMessageMutation = (
+  { __typename?: 'Mutation' }
+  & { postMessage: (
+    { __typename?: 'Message' }
+    & Pick<Message, 'id' | 'text' | 'createdAt'>
+  ) }
+);
+
+
+export const MessagesDocument = gql`
+    query Messages {
+  messages {
+    id
+    text
+    createdAt
+  }
+}
+    `;
+
+/**
+ * __useMessagesQuery__
+ *
+ * To run a query within a React component, call `useMessagesQuery` and pass it any options that fit your needs.
+ * When your component renders, `useMessagesQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useMessagesQuery({
+ *   variables: {
+ *   },
+ * });
+ */
+export function useMessagesQuery(baseOptions?: Apollo.QueryHookOptions<MessagesQuery, MessagesQueryVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useQuery<MessagesQuery, MessagesQueryVariables>(MessagesDocument, options);
+      }
+export function useMessagesLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<MessagesQuery, MessagesQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useLazyQuery<MessagesQuery, MessagesQueryVariables>(MessagesDocument, options);
+        }
+export type MessagesQueryHookResult = ReturnType<typeof useMessagesQuery>;
+export type MessagesLazyQueryHookResult = ReturnType<typeof useMessagesLazyQuery>;
+export type MessagesQueryResult = Apollo.QueryResult<MessagesQuery, MessagesQueryVariables>;
+export const PostMessageDocument = gql`
+    mutation PostMessage($messageInput: MessageInput!) {
+  postMessage(messageInput: $messageInput) {
+    id
+    text
+    createdAt
+  }
+}
+    `;
+export type PostMessageMutationFn = Apollo.MutationFunction<PostMessageMutation, PostMessageMutationVariables>;
+
+/**
+ * __usePostMessageMutation__
+ *
+ * To run a mutation, you first call `usePostMessageMutation` within a React component and pass it any options that fit your needs.
+ * When your component renders, `usePostMessageMutation` returns a tuple that includes:
+ * - A mutate function that you can call at any time to execute the mutation
+ * - An object with fields that represent the current status of the mutation's execution
+ *
+ * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
+ *
+ * @example
+ * const [postMessageMutation, { data, loading, error }] = usePostMessageMutation({
+ *   variables: {
+ *      messageInput: // value for 'messageInput'
+ *   },
+ * });
+ */
+export function usePostMessageMutation(baseOptions?: Apollo.MutationHookOptions<PostMessageMutation, PostMessageMutationVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useMutation<PostMessageMutation, PostMessageMutationVariables>(PostMessageDocument, options);
+      }
+export type PostMessageMutationHookResult = ReturnType<typeof usePostMessageMutation>;
+export type PostMessageMutationResult = Apollo.MutationResult<PostMessageMutation>;
+export type PostMessageMutationOptions = Apollo.BaseMutationOptions<PostMessageMutation, PostMessageMutationVariables>;
