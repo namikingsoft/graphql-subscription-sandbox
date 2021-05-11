ee29733ebc70e0b94b3574af87ebc4ab744b0eb4
Tsubasa Namiki <namikingsoft@gmail.com>
2021-05-12 04:00:29 +0900

    Implement message entity

--- src/message/message.entity.ts
+++ src/message/message.entity.ts
@@ -1,1 +1,18 @@
-export class MessageEntity {}
+import {
+  Entity,
+  Column,
+  CreateDateColumn,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity('messages')
+export class MessageEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('text')
+  text: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+}
