1b653db9a3fb3920b4b13cc89044d5397ec3a17a
Tsubasa Namiki <namikingsoft@gmail.com>
2021-05-12 07:30:45 +0900

    Implement frontend

--- src/client/pages/_app.tsx
+++ src/client/pages/_app.tsx
@@ -1,18 +1,35 @@
 import React from 'react';
 import App from 'next/app';
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  HttpLink,
+} from '@apollo/client';
 import { CssBaseline, createMuiTheme, ThemeProvider } from '@material-ui/core';
 
 const theme = createMuiTheme({});
+
+const httpLink = new HttpLink({
+  uri: '/graphql',
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
 
 export default class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
 
     return (
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <ApolloProvider client={client}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </ApolloProvider>
     );
   }
 }
--- src/client/pages/index.tsx
+++ src/client/pages/index.tsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { FormEvent } from 'react';
 import { NextPage } from 'next';
 import Head from 'next/head';
-import { makeStyles, Container } from '@material-ui/core';
+import {
+  makeStyles,
+  Container,
+  TextField,
+  Button,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemAvatar,
+  Avatar,
+} from '@material-ui/core';
+import { Image } from '@material-ui/icons/';
+import { useMessagesQuery, usePostMessageMutation } from '../lib/api';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -12,12 +24,56 @@ const useStyles = makeStyles((theme) => ({
 const Home: NextPage = () => {
   const classes = useStyles();
 
+  const resultMessages = useMessagesQuery();
+  const [postMessage, resultPostMessage] = usePostMessageMutation();
+
+  const [text, setText] = React.useState('');
+
+  const onSubmit = React.useCallback(
+    (event: FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
+      postMessage({ variables: { messageInput: { text } } });
+    },
+    [text, postMessage],
+  );
+
   return (
     <div className={classes.root}>
       <Head>
         <title>App</title>
       </Head>
-      <Container>Hello</Container>
+      <Container>
+        <List>
+          {!resultMessages.loading &&
+            resultMessages.data.messages.map((message) => (
+              <ListItem>
+                <ListItemAvatar>
+                  <Avatar>
+                    <Image />
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText
+                  primary={message.text}
+                  secondary={String(new Date(message.createdAt))}
+                />
+              </ListItem>
+            ))}
+        </List>
+        <form onSubmit={onSubmit}>
+          <TextField
+            label="Standard"
+            onChange={({ target }) => setText(target.value)}
+          />
+          <Button
+            type="submit"
+            color="primary"
+            variant="contained"
+            disabled={resultPostMessage.loading}
+          >
+            Post
+          </Button>
+        </form>
+      </Container>
     </div>
   );
 };
