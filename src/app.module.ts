cee5df7858b61e0d5934bf4fa1061528dccdbb02
Tsubasa Namiki <namikingsoft@gmail.com>
2021-05-12 03:20:06 +0900

    Generate message module
    ```bash
    nest g mo message
    nest g r message
    nest g s message
    nest g cl message.entity
    nest g cl message.model
    ```

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MessageModule } from './message/message.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot()],
+  imports: [TypeOrmModule.forRoot(), MessageModule],
   controllers: [AppController],
   providers: [AppService],
 })
